e3c8776cd877b27f8fda8872506d71913513b022
Guilherme Pereira do Vale <134980831+Marrow4620@users.noreply.github.com>
2024-08-20 21:31:19 -0300

    Update FarenToCelsi.java

--- FarenToCelsi.java
+++ FarenToCelsi.java
@@ -4,9 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Digite um valor em Fahrenheit ");
-            double valor = input.nextDouble() -32;
-            double resultado = valor/ 1.8;
-            int casting = (int) resultado;
+            double valor = (input.nextDouble() -32)/1.8;
+            int casting = (int) valor;
             System.out.println("O resultado é " + casting);
 
     }
